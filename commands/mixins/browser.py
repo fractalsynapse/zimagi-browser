39be2788549c43a7e2b6c8ee2652b9eea72b94fd
Adrian Webb <adrian@webb.sh>
2024-04-03 09:39:27 -0400

    Updating the parse_webpage method in the browser command mixin.

--- commands/mixins/browser.py
+++ commands/mixins/browser.py
@@ -2,12 +2,21 @@ from systems.commands.index import CommandMixin
 from utility.data import Collection
 from bs4 import BeautifulSoup
 
+import re
+
 
 class BrowserCommandMixin(CommandMixin('browser')):
 
     def parse_webpage(self, url):
         webpage = self.submit('agent:browser', url)
+        soup = BeautifulSoup(webpage['source'], 'html.parser')
+        text = ''
+
+        if webpage['source']:
+            text = re.sub(r'\n+', '\n', soup.get_text("\n")).encode('ascii', errors = 'ignore').decode().strip()
+
         return Collection(
             url = webpage['url'],
-            soup = BeautifulSoup(webpage['source'], 'html.parser')
+            text = text,
+            soup = soup
         )
