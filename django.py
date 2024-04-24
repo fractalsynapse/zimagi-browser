3bcb987cb9600f92633d840ba738ecf0f7c7418a
Adrian Webb <adrian@webb.sh>
2024-04-23 22:50:19 -0400

    Removing the default filtered domains in the Django settings file.

--- django.py
+++ django.py
@@ -5,24 +5,4 @@ from settings.config import Config
 #
 # WebCrawler filtered domains
 #
-WEBCRAWLER_FILTERED_DOMAINS = Config.list('ZIMAGI_WEBCRAWLER_FILTERED_DOMAINS', [
-    'google.com',
-    'bing.com',
-    'perplexity.ai',
-    'you.com',
-    'yep.com',
-    'openverse.org',
-    'yahoo.com',
-    'aol.com',
-    'duckduckgo.com',
-    'startpage.com',
-    'brave.com',
-    'wiki.com',
-    'wikipedia.com',
-    'x.com',
-    'twitter.com',
-    'facebook.com',
-    'instagram.com',
-    'slideshare.net',
-    'linkedin.com'
-])
+WEBCRAWLER_FILTERED_DOMAINS = Config.list('ZIMAGI_WEBCRAWLER_FILTERED_DOMAINS', [])
