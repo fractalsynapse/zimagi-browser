ab89ad381adbeae42d2e4aeda4c18758240d8995
Adrian Webb <adrian@webb.sh>
2024-07-19 02:25:32 -0400

    Updating the browser command mixin to support direct request (non JS) in the case of failure of the Chrome browser agent request.

--- commands/mixins/browser.py
+++ commands/mixins/browser.py
@@ -1,5 +1,6 @@
 from systems.commands.index import CommandMixin
 from utility.data import Collection
+from utility.web import WebParser
 from bs4 import BeautifulSoup
 
 import re
@@ -8,12 +9,14 @@ import re
 class BrowserCommandMixin(CommandMixin('browser')):
 
     def parse_webpage(self, url):
-        webpage = self.submit('agent:browser', url)
-        soup = BeautifulSoup(webpage['source'], 'html.parser')
+        webpage = self.submit('agent:browser', url, timeout = 30)
         text = ''
 
         if webpage['source']:
-            text = re.sub(r'\n+', '\n', soup.get_text("\n")).encode('ascii', errors = 'ignore').decode().strip()
+            soup, text = self._parse_webpage_text(webpage['source'])
+        if not text:
+            browser = WebParser(url, verify = False)
+            soup, text = self._parse_webpage_text(browser.text)
 
         return Collection(
             url = webpage['url'],
@@ -21,3 +24,8 @@ class BrowserCommandMixin(CommandMixin('browser')):
             text = text,
             soup = soup
         )
+
+    def _parse_webpage_text(self, text):
+        soup = BeautifulSoup(text, 'html.parser')
+        text = re.sub(r'\n+', '\n', soup.get_text("\n")).encode('ascii', errors = 'ignore').decode().strip()
+        return soup, text
